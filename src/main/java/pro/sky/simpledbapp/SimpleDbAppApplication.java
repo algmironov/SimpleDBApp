3ff78400443155d6b787e026611107b46afddd9f
Alexey Mironov <al.g.mironov@yandex.ru>
2022-06-03 21:17:29 +0300

    Initial commit

---
+++ src/main/java/pro/sky/simpledbapp/SimpleDbAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.simpledbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDbAppApplication.class, args);
+	}
+
+}
